f0509b7e405170acceca04f990fb08b9a95e1c8a
KristapsN <kristaps.nogobods@gmail.com>
2020-12-01 16:07:49 +0200

    cc

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
 import 'flexboxgrid';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from 'react-router-dom';
 import Home from './pages/home';
 
